179e1db9312e66f5e78db41fe8a6a6c9ddbf800c
Mostafa Atef <mostafaatefsaad705@gmail.com>
2026-04-27 09:09:24 +0300

    Set SQL connection string environment variable in integration test factory

--- tests/SocialMedia.IntegrationTests/Infrastructure/IntegrationTestWebAppFactory.cs
+++ tests/SocialMedia.IntegrationTests/Infrastructure/IntegrationTestWebAppFactory.cs
@@ -26,11 +26,12 @@ namespace SocialMedia.IntegrationTests.Infrastructure;
 
 public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
+    private const string SqlConnectionStringEnvKey = "ConnectionStrings__sqlserverConnectionString";
     private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder("mcr.microsoft.com/mssql/server:2022-latest")
         .WithCleanUp(true)
         .Build();
     public HttpClient HttpClient { get; private set; } = null!;
-    private SqlConnection _dbConnection;
+    private SqlConnection _dbConnection = null!;
     private Respawner _respawner = null!;
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
@@ -63,6 +64,7 @@ public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsy
     public async ValueTask InitializeAsync()
     {
         await _msSqlContainer.StartAsync();
+        Environment.SetEnvironmentVariable(SqlConnectionStringEnvKey, _msSqlContainer.GetConnectionString());
         HttpClient = CreateClient();
         _dbConnection = new SqlConnection(_msSqlContainer.GetConnectionString());
         await _dbConnection.OpenAsync();
@@ -88,5 +90,6 @@ public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsy
         await _dbConnection.DisposeAsync();
         await base.DisposeAsync();
         await _msSqlContainer.DisposeAsync();
+        Environment.SetEnvironmentVariable(SqlConnectionStringEnvKey, null);
     }
 }
